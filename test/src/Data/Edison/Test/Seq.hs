f2a9789261ba910d8a74a07f7bc59c1f27ab065a
Bill Hallahan <whallahan@binhamton.edu>
2026-06-24 12:27:36 -0400

    running g2 on seq properties

--- test/src/Data/Edison/Test/Seq.hs
+++ test/src/Data/Edison/Test/Seq.hs
@@ -16,6 +16,9 @@ import Data.Edison.Test.Utils
 import Test.QuickCheck hiding( (===) )
 import Test.HUnit (Test(..))
 
+import G2.Plugin hiding ((==>))
+import qualified G2.Plugin as G2
+
 ------------------------------------------------------
 -- The sequence implementations to check
 
@@ -30,6 +33,7 @@ import qualified Data.Edison.Seq.RandList as RL
 import qualified Data.Edison.Seq.SimpleQueue as SQ
 import qualified Data.Edison.Seq.SizedSeq as Sized
 import qualified Data.Edison.Seq.RevSeq as Rev
+import qualified Data.Edison.Seq.Defaults as Def
 
 
 --------------------------------------------------------
@@ -56,40 +60,40 @@ instance (Eq a,Arbitrary a,Show a,Read a,SeqTest a seq) => SeqTest a (Rev.Rev se
 ---------------------------------------------------------
 -- List all permutations of sequence types to test
 
-allSequenceTests :: Test
-allSequenceTests = TestList
-   [ seqTests (empty :: [a])
-   , seqTests (empty :: Sized.Sized [] a)
-   , seqTests (empty :: Rev.Rev [] a)
-   , seqTests (empty :: BQ.Seq a)
-   , seqTests (empty :: Sized.Sized BQ.Seq a)
-   , seqTests (empty :: Rev.Rev BQ.Seq a)
-   , seqTests (empty :: BS.Seq a)
-   , seqTests (empty :: Sized.Sized BS.Seq a)
-   , seqTests (empty :: Rev.Rev BS.Seq a)
-   , seqTests (empty :: FS.Seq a)
-   , seqTests (empty :: Sized.Sized FS.Seq a)
-   , seqTests (empty :: Rev.Rev FS.Seq a)
-   , seqTests (empty :: JL.Seq a)
-   , seqTests (empty :: Sized.Sized JL.Seq a)
-   , seqTests (empty :: Rev.Rev JL.Seq a)
-   , seqTests (empty :: MS.Seq a)
-   , seqTests (empty :: Sized.Sized MS.Seq a)
-   , seqTests (empty :: Rev.Rev MS.Seq a)
-   , seqTests (empty :: RL.Seq a)
-   , seqTests (empty :: Sized.Sized RL.Seq a)
-   , seqTests (empty :: Rev.Rev RL.Seq a)
-   , seqTests (empty :: SQ.Seq a)
-   , seqTests (empty :: Sized.Sized SQ.Seq a)
-   , seqTests (empty :: Rev.Rev SQ.Seq a)
-   , seqTests (empty :: BRL.Seq a)
-   , seqTests (empty :: Sized.Sized BRL.Seq a)
-   , seqTests (empty :: Rev.Rev BRL.Seq a)
-   , seqTests (empty :: Sized.Sized (Sized.Sized SQ.Seq) a)
-   , seqTests (empty :: Rev.Rev (Rev.Rev SQ.Seq) a)
-   , seqTests (empty :: Sized.Sized (Rev.Rev SQ.Seq) a)
-   , seqTests (empty :: Rev.Rev (Sized.Sized SQ.Seq) a)
-   ]
+-- allSequenceTests :: Test
+-- allSequenceTests = TestList
+--    [ seqTests (empty :: [a])
+--    , seqTests (empty :: Sized.Sized [] a)
+--    , seqTests (empty :: Rev.Rev [] a)
+--    , seqTests (empty :: BQ.Seq a)
+--    , seqTests (empty :: Sized.Sized BQ.Seq a)
+--    , seqTests (empty :: Rev.Rev BQ.Seq a)
+--    , seqTests (empty :: BS.Seq a)
+--    , seqTests (empty :: Sized.Sized BS.Seq a)
+--    , seqTests (empty :: Rev.Rev BS.Seq a)
+--    , seqTests (empty :: FS.Seq a)
+--    , seqTests (empty :: Sized.Sized FS.Seq a)
+--    , seqTests (empty :: Rev.Rev FS.Seq a)
+--    , seqTests (empty :: JL.Seq a)
+--    , seqTests (empty :: Sized.Sized JL.Seq a)
+--    , seqTests (empty :: Rev.Rev JL.Seq a)
+--    , seqTests (empty :: MS.Seq a)
+--    , seqTests (empty :: Sized.Sized MS.Seq a)
+--    , seqTests (empty :: Rev.Rev MS.Seq a)
+--    , seqTests (empty :: RL.Seq a)
+--    , seqTests (empty :: Sized.Sized RL.Seq a)
+--    , seqTests (empty :: Rev.Rev RL.Seq a)
+--    , seqTests (empty :: SQ.Seq a)
+--    , seqTests (empty :: Sized.Sized SQ.Seq a)
+--    , seqTests (empty :: Rev.Rev SQ.Seq a)
+--    , seqTests (empty :: BRL.Seq a)
+--    , seqTests (empty :: Sized.Sized BRL.Seq a)
+--    , seqTests (empty :: Rev.Rev BRL.Seq a)
+--    , seqTests (empty :: Sized.Sized (Sized.Sized SQ.Seq) a)
+--    , seqTests (empty :: Rev.Rev (Rev.Rev SQ.Seq) a)
+--    , seqTests (empty :: Sized.Sized (Rev.Rev SQ.Seq) a)
+--    , seqTests (empty :: Rev.Rev (Sized.Sized SQ.Seq) a)
+--    ]
 
 
 ---------------------------------------------------------------
@@ -97,44 +101,44 @@ allSequenceTests = TestList
 
 -- infer the type so I don't have to write down the big nasty
 -- class context...
-seqTests seq = TestLabel ("Sequence test "++(instanceName seq)) . TestList $
-  [ qcTest $ prop_equals seq
-  , qcTest $ prop_fromList seq
-  , qcTest $ prop_toList seq
-  , qcTest $ prop_single seq
-  , qcTest $ prop_lcons_rcons seq
-  , qcTest $ prop_lview_rview seq
-  , qcTest $ prop_lhead_rhead seq
-  , qcTest $ prop_ltail_rtail seq
-  , qcTest $ prop_append seq
-  , qcTest $ prop_null_size seq
-  , qcTest $ prop_reverse seq                      -- 10
-  , qcTest $ prop_reverseOnto seq
-  , qcTest $ prop_map seq
-  , qcTest $ prop_fold seq
-  , qcTest $ prop_strict_fold seq
-  , qcTest $ prop_fold1 seq
-  , qcTest $ prop_strict_fold1 seq
-  , qcTest $ prop_reduce seq
-  , qcTest $ prop_strict_reduce seq
-  , qcTest $ prop_reduce1 seq
-  , qcTest $ prop_strict_reduce1 seq               -- 20
-  , qcTest $ prop_inBounds_lookup seq
-  , qcTest $ prop_update_adjust seq
-  , qcTest $ prop_withIndex seq
-  , qcTest $ prop_take_drop_splitAt seq
-  , qcTest $ prop_subseq seq
-  , qcTest $ prop_filter_takeWhile_dropWhile seq
-  , qcTest $ prop_partition_splitWhile seq
-  , qcTest $ prop_zip_zipWith seq
-  , qcTest $ prop_zip3_zipWith3 seq
-  , qcTest $ prop_unzip_unzipWith seq              -- 30
-  , qcTest $ prop_unzip3_unzipWith3 seq
-  , qcTest $ prop_concat seq
-  , qcTest $ prop_concatMap seq
-  , qcTest $ prop_strict seq
-  , qcTest $ prop_show_read seq
-  ]
+-- seqTests seq = TestLabel ("Sequence test "++(instanceName seq)) . TestList $
+--   [ qcTest $ prop_equals seq
+--   , qcTest $ prop_fromList seq
+--   , qcTest $ prop_toList seq
+--   , qcTest $ prop_single seq
+--   , qcTest $ prop_lcons_rcons seq
+--   , qcTest $ prop_lview_rview seq
+--   , qcTest $ prop_lhead_rhead seq
+--   , qcTest $ prop_ltail_rtail seq
+--   , qcTest $ prop_append seq
+--   , qcTest $ prop_null_size seq
+--   , qcTest $ prop_reverse seq                      -- 10
+--   , qcTest $ prop_reverseOnto seq
+--   , qcTest $ prop_map seq
+--   , qcTest $ prop_fold seq
+--   , qcTest $ prop_strict_fold seq
+--   , qcTest $ prop_fold1 seq
+--   , qcTest $ prop_strict_fold1 seq
+--   , qcTest $ prop_reduce seq
+--   , qcTest $ prop_strict_reduce seq
+--   , qcTest $ prop_reduce1 seq
+--   , qcTest $ prop_strict_reduce1 seq               -- 20
+--   , qcTest $ prop_inBounds_lookup seq
+--   , qcTest $ prop_update_adjust seq
+--   , qcTest $ prop_withIndex seq
+--   , qcTest $ prop_take_drop_splitAt seq
+--   , qcTest $ prop_subseq seq
+--   , qcTest $ prop_filter_takeWhile_dropWhile seq
+--   , qcTest $ prop_partition_splitWhile seq
+--   , qcTest $ prop_zip_zipWith seq
+--   , qcTest $ prop_zip3_zipWith3 seq
+--   , qcTest $ prop_unzip_unzipWith seq              -- 30
+--   , qcTest $ prop_unzip3_unzipWith3 seq
+--   , qcTest $ prop_concat seq
+--   , qcTest $ prop_concatMap seq
+--   , qcTest $ prop_strict seq
+--   , qcTest $ prop_show_read seq
+--   ]
 
 (===) :: (Eq (seq a),Sequence seq) => seq a -> seq a -> Bool
 (===) s1 s2 = 
@@ -155,7 +159,7 @@ prop_equals :: SeqTest Int seq => seq Int -> seq Int -> seq Int -> Bool
 prop_equals seq xs ys =
     si xs && si ys && (xs == ys) == (toList xs == toList ys)
 
-prop_fromList :: SeqTest Int seq => seq Int -> [Int] -> Bool
+prop_fromList :: BQ.Seq Int -> [Int] -> Bool
 prop_fromList seq xs =
     fromList xs === (Prelude.foldr lcons empty xs `asTypeOf` seq)
     &&
@@ -232,49 +236,80 @@ prop_reverseOnto :: SeqTest Int seq => seq Int -> seq Int -> seq Int -> Bool
 prop_reverseOnto seq xs ys =
     reverseOnto xs ys === append (reverse xs) ys
 
+-- {-# ANN prop_map (SymExWithConfig "--no-step-limit --time 720 --higher-order symbolic --search subpath --subpath-len 8 --returns-true --accept-times --sum-height-limit 20 --print-timeout-list-depth --smt cvc5 --states-at-time")
+--    #-}
+{-# ANN prop_bq_map (SymExWithConfig "--no-step-limit --time 720 --higher-order symbolic --search subpath --subpath-len 8 --returns-true --accept-times --sum-height-limit 20 --print-timeout-list-depth --smt cvc5 --states-at-time")
+    #-}
+prop_bq_map :: (Int -> Int) -> BQ.Seq Int -> BQ.Seq Int -> Bool
+prop_bq_map = prop_map
 
-prop_map :: SeqTest Int seq => seq Int -> seq Int -> Bool
-prop_map seq xs =
-    let succ_xs = map (+1) xs
-     in si succ_xs
-        &&
-       toList succ_xs == Prelude.map (+1) (toList xs)
+prop_map :: SeqTest Int seq => (Int -> Int) -> seq Int -> seq Int -> Bool
+prop_map f seq xs = 
+    si xs G2.==>
+            let succ_xs = map f {- (+1) -} xs
+            in si succ_xs
+                &&
+            toList succ_xs == Prelude.map f {- (+1) -} (toList xs)
 
-prop_fold :: SeqTest Int seq => seq Int -> seq Int -> Bool
-prop_fold seq xs =
-    foldr (:) [99] xs == toList xs ++ [99]
-    &&
-    foldl (flip (:)) [99] xs == Prelude.reverse (toList xs) ++ [99]
-    &&
-    fold (+) 0 xs == foldr (+) 0 xs
-    &&
-    fold' (+) 0 xs == foldr' (+) 0 xs
-    &&
-    if (not . null) xs then fold1 (+) xs == foldr1 (+) xs else True
-    &&
-    if (not . null) xs then fold1' (+) xs == foldr1 (+) xs else True
+-- {-# ANN prop_bq_fold (SymExWithConfig "--no-step-limit --time 720 --higher-order symbolic --search subpath --subpath-len 8 --returns-true --accept-times --sum-height-limit 20 --print-timeout-list-depth --smt cvc5 --states-at-time")
+--     #-}
+prop_bq_fold :: (Int -> Int -> Int) -> BQ.Seq Int -> BQ.Seq Int -> Bool
+prop_bq_fold = prop_fold
 
-prop_strict_fold :: SeqTest Int seq => seq Int -> seq Int -> Bool
-prop_strict_fold seq xs =
-    foldr (+) 0 xs == foldr' (+) 0 xs
-    &&
-    foldl (+) 0 xs == foldl' (+) 0 xs
+prop_fold :: SeqTest Int seq => (Int -> Int -> Int) -> seq Int -> seq Int -> Bool
+prop_fold f seq xs =
+    -- foldr (:) [99] xs == toList xs ++ [99]
+    -- &&
+    -- foldl (flip (:)) [99] xs == Prelude.reverse (toList xs) ++ [99]
+    -- &&
+        si xs G2.==>
+            fold f' 0 xs == foldr f 0 xs
+            &&
+            fold' f' 0 xs == foldr' f 0 xs
+            &&
+            if (not . null) xs then fold1 f' xs == foldr1 f xs else True
+            &&
+            if (not . null) xs then fold1' f' xs == foldr1 f xs else True
+            where
+                f' x y = assume (f x y == f y x) (f x y)
 
-prop_fold1 :: SeqTest Int seq => seq Int -> seq Int -> Property
-prop_fold1 seq xs =
-    not (null xs) ==>
-       foldr1 f xs == Prelude.foldr1 f (toList xs)
-       &&
-       foldl1 f xs == Prelude.foldl1 f (toList xs)
-  where f x y = 3*x - 2*y
+{-# ANN prop_bq_strict_fold (SymExWithConfig "--no-step-limit --time 720 --higher-order symbolic --search subpath --subpath-len 8 --returns-true --accept-times --sum-height-limit 20 --print-timeout-list-depth --smt cvc5 --states-at-time")
+    #-}
+prop_bq_strict_fold :: (Int -> Int -> Int) -> BQ.Seq Int -> BQ.Seq Int -> Bool
+prop_bq_strict_fold = prop_strict_fold
 
-prop_strict_fold1 :: SeqTest Int seq => seq Int -> seq Int -> Property
-prop_strict_fold1 seq xs =
-    not (null xs) ==>
-       foldr1' f xs == foldr1 f xs
-       &&
-       foldl1' f xs == foldl1 f xs
-  where f x y = 3*x - 2*y
+prop_strict_fold :: SeqTest Int seq => (Int -> Int -> Int) -> seq Int -> seq Int -> Bool
+prop_strict_fold f seq xs =
+        si xs G2.==>
+            foldr f 0 xs == foldr' f 0 xs
+            &&
+            foldl f 0 xs == foldl' f 0 xs
+
+{-# ANN prop_bq_fold1 (SymExWithConfig "--no-step-limit --time 720 --higher-order symbolic --search subpath --subpath-len 8 --returns-true --accept-times --sum-height-limit 20 --print-timeout-list-depth --smt cvc5 --states-at-time")
+    #-}
+prop_bq_fold1 :: (Int -> Int -> Int) -> BQ.Seq Int -> BQ.Seq Int -> Bool
+prop_bq_fold1 = prop_fold1
+
+prop_fold1 :: SeqTest Int seq => (Int -> Int -> Int) -> seq Int -> seq Int -> Bool
+prop_fold1 f seq xs =
+        si xs G2.==>
+            not (null xs) G2.==>
+                foldr1 f xs == Prelude.foldr1 f (toList xs)
+                &&
+                foldl1 f xs == Prelude.foldl1 f (toList xs)
+
+{-# ANN prop_bq_strict_fold1 (SymExWithConfig "--no-step-limit --time 720 --higher-order symbolic --search subpath --subpath-len 8 --returns-true --accept-times --sum-height-limit 20 --print-timeout-list-depth --smt cvc5 --states-at-time")
+    #-}
+prop_bq_strict_fold1 :: (Int -> Int -> Int) -> BQ.Seq Int -> BQ.Seq Int -> Bool
+prop_bq_strict_fold1 = prop_strict_fold1
+
+prop_strict_fold1 :: SeqTest Int seq => (Int -> Int -> Int) -> seq Int -> seq Int -> Bool
+prop_strict_fold1 f seq xs =
+        si xs G2.==>
+            not (null xs) G2.==>
+            foldr1' f xs == foldr1 f xs
+            &&
+            foldl1' f xs == foldl1 f xs
 
 prop_reduce :: SeqTest Int seq => seq Int -> seq Int -> Bool
 prop_reduce seq xs =
@@ -314,33 +349,44 @@ prop_inBounds_lookup seq i xs =
        &&
        lookupWithDefault 99 i xs == 99)
 
+{-# ANN prop_bq_update_adjust (SymExWithConfig "--no-step-limit --time 720 --higher-order symbolic --search subpath --subpath-len 8 --returns-true --accept-times --sum-height-limit 20 --print-timeout-list-depth --smt cvc5 --states-at-time")
+    #-}
+prop_bq_update_adjust :: (Int -> Int) -> BQ.Seq Int -> Int -> BQ.Seq Int -> Bool
+prop_bq_update_adjust = prop_update_adjust
 
-prop_update_adjust :: SeqTest Int seq => seq Int -> Int -> seq Int -> Bool
-prop_update_adjust seq i xs =
-    if inBounds i xs then
-      let ys = take i xs
-          zs = drop (i+1) xs
-          x = lookup i xs
-      in
-        si ys 
+prop_update_adjust :: SeqTest Int seq => (Int -> Int) -> seq Int -> Int -> seq Int -> Bool
+prop_update_adjust f seq i xs =
+    si xs G2.==>
+        if inBounds i xs then
+        let ys = take i xs
+            zs = drop (i+1) xs
+            x = lookup i xs
+        in
+            si ys 
+            &&
+            si zs
+            &&
+            update i 99 xs == append ys (lcons 99 zs)
+            &&
+            adjust f i xs == append ys (lcons (f x) zs)
+        else
+        update i 99 xs === xs
         &&
-        si zs
-        &&
-        update i 99 xs == append ys (lcons 99 zs)
-        &&
-        adjust (+1) i xs == append ys (lcons (x+1) zs)
-    else
-      update i 99 xs === xs
-      &&
-      adjust (+1) i xs === xs
+        adjust f i xs === xs
 
-prop_withIndex :: SeqTest Int seq => seq Int -> seq Int -> Bool
-prop_withIndex seq xs =
-    toList (mapWithIndex (+) xs) == Prelude.map (uncurry (+)) ixs
-    &&
-    foldrWithIndex f [] xs == ixs
-    &&
-    foldlWithIndex g [] xs == Prelude.reverse ixs
+{-# ANN prop_bq_withIndex (SymExWithConfig "--no-step-limit --time 720 --higher-order symbolic --search subpath --subpath-len 8 --returns-true --accept-times --sum-height-limit 20 --print-timeout-list-depth --smt cvc5 --states-at-time")
+    #-}
+prop_bq_withIndex :: (Int -> Int -> Int) -> BQ.Seq Int -> BQ.Seq Int -> Bool
+prop_bq_withIndex = prop_withIndex
+
+prop_withIndex :: SeqTest Int seq => (Int -> Int -> Int) -> seq Int -> seq Int -> Bool
+prop_withIndex h seq xs =
+    si xs G2.==>
+        toList (mapWithIndex h xs) == Prelude.map (uncurry h) ixs
+        &&
+        foldrWithIndex f [] xs == ixs
+        &&
+        foldlWithIndex g [] xs == Prelude.reverse ixs
   where ixs = Prelude.zip [0..] (toList xs)
         f i x xs = (i,x):xs
         g xs i x = (i,x):xs
@@ -357,33 +403,46 @@ prop_subseq :: SeqTest Int seq => seq Int -> Int -> Int -> seq Int -> Bool
 prop_subseq seq i len xs =
     subseq i len xs === take len (drop i xs)
 
-prop_filter_takeWhile_dropWhile :: SeqTest Int seq =>
-        seq Int -> Int -> seq Int -> Bool
+{-# ANN prop_bq_filter_takeWhile_dropWhile (SymExWithConfig "--no-step-limit --time 720 --higher-order symbolic --search subpath --subpath-len 8 --returns-true --accept-times --sum-height-limit 20 --print-timeout-list-depth --smt cvc5 --states-at-time")
+    #-}
+prop_bq_filter_takeWhile_dropWhile :: (Int -> Bool) -> BQ.Seq Int -> Int -> BQ.Seq Int -> Bool
+prop_bq_filter_takeWhile_dropWhile = prop_filter_takeWhile_dropWhile
 
-prop_filter_takeWhile_dropWhile seq x xs =
-    toList (filter p xs) == Prelude.filter p (toList xs)
-    &&
-    toList (takeWhile p xs) == Prelude.takeWhile p (toList xs)
-    &&
-    toList (dropWhile p xs) == Prelude.dropWhile p (toList xs)
-  where p = (< x)
+prop_filter_takeWhile_dropWhile :: SeqTest Int seq => (Int -> Bool) -> seq Int -> Int -> seq Int -> Bool
+prop_filter_takeWhile_dropWhile p seq x xs =
+        si xs G2.==>
+            toList (filter p xs) == Prelude.filter p (toList xs)
+            &&
+            toList (takeWhile p xs) == Prelude.takeWhile p (toList xs)
+            &&
+            toList (dropWhile p xs) == Prelude.dropWhile p (toList xs)
+--   where p = (< x)
 
-prop_partition_splitWhile :: SeqTest Int seq =>
-        seq Int -> Int -> seq Int -> Bool
+{-# ANN prop_bq_partition_splitWhile (SymExWithConfig "--no-step-limit --time 720 --higher-order symbolic --search subpath --subpath-len 8 --returns-true --accept-times --sum-height-limit 20 --print-timeout-list-depth --smt cvc5 --states-at-time")
+    #-}
+prop_bq_partition_splitWhile :: (Int -> Bool) -> BQ.Seq Int -> Int -> BQ.Seq Int -> Bool
+prop_bq_partition_splitWhile = prop_partition_splitWhile
 
-prop_partition_splitWhile seq x xs =
-    partition p xs == (filter p xs, filter (not . p) xs)
-    &&
-    splitWhile p xs == (takeWhile p xs, dropWhile p xs)
-  where p = (< x)
+prop_partition_splitWhile :: SeqTest Int seq => (Int -> Bool) -> seq Int -> Int -> seq Int -> Bool
+prop_partition_splitWhile p seq x xs =
+        si xs G2.==>
+            partition p xs == (filter p xs, filter (not . p) xs)
+            &&
+            splitWhile p xs == (takeWhile p xs, dropWhile p xs)
+--   where p = (< x)
 
+{-# ANN prop_bq_zip_zipWith (SymExWithConfig "--no-step-limit --time 720 --higher-order symbolic --search subpath --subpath-len 8 --returns-true --accept-times --sum-height-limit 20 --print-timeout-list-depth --smt cvc5 --states-at-time")
+    #-}
+prop_bq_zip_zipWith :: (Int -> Int -> Int) -> BQ.Seq Int -> BQ.Seq Int -> BQ.Seq Int -> Bool
+prop_bq_zip_zipWith = prop_zip_zipWith
 
-prop_zip_zipWith :: SeqTest Int seq => seq Int -> seq Int -> seq Int -> Bool
-prop_zip_zipWith seq xs ys =
-    toList (zip xs ys) == xys
-    &&
-    toList (zipWith (,) xs ys) == xys
-  where xys = Prelude.zip (toList xs) (toList ys)
+prop_zip_zipWith :: SeqTest Int seq => (Int -> Int -> Int) -> seq Int -> seq Int -> seq Int -> Bool
+prop_zip_zipWith f seq xs ys =
+    -- toList (zip xs ys) == xys
+    -- &&
+    (si xs && si ys) G2.==>
+        toList (zipWith f xs ys) == xys
+  where xys = Prelude.zipWith f (toList xs) (toList ys)
 
 prop_zip3_zipWith3 :: SeqTest Int seq =>
         seq Int -> seq Int -> seq Int -> seq Int -> Bool
@@ -444,12 +503,20 @@ genss seq = sized (\n -> resize (min 20 n) arbitrary)
 
 
 
-prop_concatMap :: (SeqTest (seq Int) seq, SeqTest Int seq) =>
-        seq Int -> seq Int -> Property
+{-# ANN prop_bq_concatMap (SymExWithConfig "--no-step-limit --time 720 --higher-order symbolic --search subpath --subpath-len 8 --returns-true --accept-times --sum-height-limit 20 --print-timeout-list-depth --smt cvc5 --states-at-time")
+    #-}
+prop_bq_concatMap :: (Int -> BQ.Seq Int) -> BQ.Seq Int -> BQ.Seq Int -> Bool
+prop_bq_concatMap = prop_concatMap
 
-prop_concatMap seq xs = forAll (genss seq) check
-  where check xss = concatMap f xs === concat (map f xs)
-            where f i = lookupWithDefault empty i xss
+prop_concatMap :: SeqTest Int seq => (Int -> seq Int) -> seq Int -> seq Int -> Bool
+prop_concatMap f seq xs =
+        si xs G2.==>
+            concatMap f' xs === concat (map f' xs)
+    where
+        f' ys = let r = f ys in G2.assume (si r) r
+-- prop_concatMap seq xs = forAll (genss seq) check
+--   where check xss = concatMap f xs === concat (map f xs)
+--             where f i = lookupWithDefault empty i xss
 
 prop_strict :: SeqTest Int seq =>
         seq Int -> seq Int -> Bool
